d990bdc30b51074be92911fecda24590580d95d5
RomanMykush <roman.mykush.dev@gmail.com>
2023-10-08 17:59:31 +0300

    Post-initial commit

--- ClientApp/proxy.dev.conf.js
+++ ClientApp/proxy.dev.conf.js
@@ -1,12 +1,12 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:55014';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:31944';
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/api/*",
    ],
     proxyTimeout: 10000,
     target: target,
