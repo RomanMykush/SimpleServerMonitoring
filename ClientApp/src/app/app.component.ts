f98fd1e4db74714d309040c0c72dbfba06c30fa7
RomanMykush <roman.mykush.dev@gmail.com>
2023-12-09 22:27:11 +0200

    Fixed private key input bug

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: ['']
+  templateUrl: './app.component.html'
 })
 export class AppComponent { }
